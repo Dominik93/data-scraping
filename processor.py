ca817a64f954e35a4d3124964e838216d1e4d83f
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-20 17:51:04 +0200

    processing, support expression better processing tests

--- processor.py
+++ processor.py
@@ -1,6 +1,12 @@
 from dict_util import _get_path_or_default
 
 
+class ProcessingException(Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
+
+
 def _predicate(predicate: dict, value, default):
     for key in predicate:
         if value in predicate[key]:
@@ -12,28 +18,36 @@ def _template(template: str, value):
     return template.replace("{value}", value)
 
 
-# todo
-def _expression(expression: str, item):
-    return eval(expression)
+def _expression(expression: str, current_item, default):
+    try:
+        current = current_item['current']
+        return eval(expression)
+    except Exception as e:
+        print(f"Warning: eval {expression} failed. Error: {e}")
+        return default
 
 
-def _process(item, characteristic_provider):
-    if 'template' in characteristic_provider:
+def _process(item: dict, characteristic_provider):
+    try:
+        if 'template' in characteristic_provider:
+            default = characteristic_provider['default']
+            path = characteristic_provider['path']
+            value = _get_path_or_default(item, path, default)
+            return _template(characteristic_provider['template'], value)
+        if 'predicate' in characteristic_provider:
+            default = characteristic_provider['default']
+            path = characteristic_provider['path']
+            value = _get_path_or_default(item, path, default)
+            return _predicate(characteristic_provider['predicate'], value, default)
+        if 'expression' in characteristic_provider:
+            default = characteristic_provider['default']
+            return _expression(characteristic_provider['expression'], item, default)
+
         default = characteristic_provider['default']
         path = characteristic_provider['path']
-        value = _get_path_or_default(item, path, default)
-        return _template(characteristic_provider['template'], value)
-    if 'predicate' in characteristic_provider:
-        default = characteristic_provider['default']
-        path = characteristic_provider['path']
-        value = _get_path_or_default(item, path, default)
-        return _predicate(characteristic_provider['predicate'], value, default)
-    if 'expression' in characteristic_provider:
-        return _expression(characteristic_provider['expression'], item)
-
-    default = characteristic_provider['default']
-    path = characteristic_provider['path']
-    return _get_path_or_default(item, path, default)
+        return _get_path_or_default(item, path, default)
+    except Exception as e:
+        raise ProcessingException(f"Error when processing {item['id']} with {characteristic_provider}", e)
 
 
 def process_items(processor_config, items: list[dict]) -> list[dict]:
--- tests/processor_test.py
+++ tests/processor_test.py
@@ -2,37 +2,70 @@ import unittest
 
 from processor import process_items
 
-processor_config = {
-    "title": {"path": "$item.title", "default": "-"},
-    "description": {"path": "$item.description", "default": "-"},
-    "last": {"path": "$item.items.[-1]", "default": "-"},
-    "link": {"path": "$item.title", "template": "http://{value}", "default": "-"},
-    "selected": {"path": "$item.title", "predicate": {
-        "Y": ["selected_one", "selected_two"],
-        "N": ["not_selected"]
-    }, "default": ""},
-    "value": {"path": "$item.value", "default": 0},
-    #"calculated_value": {"expression": "round( current['value'], 2 )"}
-}
-
 
 class ProcessorTestCase(unittest.TestCase):
-    def test_proces_items(self):
-        samples = []
-        samples.append({"item": {"title": "sample title", "value": 1, "items": ["one"]}})
-        samples.append({"item": {"title": "selected_one", "value": 2.5,"items": []}})
-        samples.append({"item": {"title": "selected_two", "value": 5,"items": ["one", "two"]}})
-        samples.append({"item": {"title": "not_selected"}})
+    def test_process_items_with_expression(self):
+        processor_config = {"value": {"path": "$item.value", "default": 0},
+                            "calculated_value": {"expression": "round(current['value'], 0)"}}
+        samples = [{"item": {"value": 2.3}}]
         actual = process_items(processor_config, samples)
-        expected = []
-        expected.append(
-            {"title": "sample title", "description": "-", "last": "one", "link": "http://sample title", "selected": "", "value":1})
-        expected.append(
-            {"title": "selected_one", "description": "-", "last": "-", "link": "http://selected_one", "selected": "Y", "value":2.5})
-        expected.append({"title": "selected_two", "description": "-", "last": "two", "link": "http://selected_two",
-                         "selected": "Y", "value":5})
-        expected.append(
-            {"title": "not_selected", "description": "-", "last": "-", "link": "http://not_selected", "selected": "N", "value":0})
+        expected = [{"value": 2.3, "calculated_value": 2}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_template(self):
+        processor_config = {"link": {"path": "$item.sample", "template": "http://{value}", "default": "-"}}
+        samples = [{"item": {"sample": "value"}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"link": "http://value"}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_predicate_default_value(self):
+        processor_config = {
+            "selected": {"path": "$item.sample", "predicate": {
+                "Y": ["selected_one", "selected_two"],
+                "N": ["not_selected"]
+            }, "default": "-"}}
+        samples = [{"item": {}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"selected": "-"}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_predicate(self):
+        processor_config = {"selected": {"path": "$item.sample", "predicate": {
+            "Y": ["selected_one", "selected_two"],
+            "N": ["not_selected"]
+        }, "default": ""}}
+        samples = [{"item": {"sample": "selected_one"}}, {"item": {"sample": "not_selected"}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"selected": "Y"}, {"selected": "N"}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_array(self):
+        processor_config = {"last": {"path": "$item.items.[-1]", "default": "-"}}
+        samples = [{"item": {"items": ["value"]}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"last": "value"}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_default_value(self):
+        processor_config = {"title": {"path": "$item.sample", "default": "-"}}
+        samples = [{"item": {}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"title": "-"}]
+
+        self.assertEqual(expected, actual)
+
+    def test_process_items_with_simple_path(self):
+        processor_config = {"title": {"path": "$item.sample", "default": "-"}}
+        samples = [{"item": {"sample": "sample title"}}]
+        actual = process_items(processor_config, samples)
+        expected = [{"title": "sample title"}]
+
         self.assertEqual(expected, actual)
 
 
